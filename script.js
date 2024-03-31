7455af9b3e1b10531d49b5d7424635729d0708f2
Marco Lunardi <marcolunardi10@gmail.com>
2024-03-31 20:02:38 -0300

    Atualizando arquivos

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 let botao = document.getElementById("tema");
-let body = document.getElementById("body");
+let body = document.body;
 
 botao.addEventListener("click", function(){
-    if(body.style.backgroundColor == "white"){
-        body.style.backgroundColor = "#212222";
-        body.style.color = "white";
-    }else{
+    if (getComputedStyle(body).backgroundColor === "rgb(33, 34, 34)"){
         body.style.backgroundColor = "white";
         body.style.color = "black";
+    }else{
+        body.style.backgroundColor = "rgb(33, 34, 34)";
+        body.style.color = "white";
     }
+});
+
+let btnCC = document.getElementById("cc");
+let btnFilo = document.getElementById("filosofia");
+let divcc = document.getElementById("divcc");
+let divFilo = document.getElementById("divfilo");
+
+btnCC.addEventListener("click", function(){
+        divcc.style.display = "block";
+        divFilo.style.display = "none";
+});
+
+btnFilo.addEventListener("click", function(){
+    divcc.style.display = "none";
+    divFilo.style.display = "block";
 });
